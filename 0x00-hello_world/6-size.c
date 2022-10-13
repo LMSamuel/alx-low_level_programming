5de27a29d875827eb4dbf99625de8d78812d5ea4
Sam2968 <masoebesamuel@gmail.com>
2022-10-13 12:38:06 -0700

    4.printf size of int,char,float

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,6 +14,7 @@ int main(void)
 	long long int c;
 	char x;
 	float y;
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(x));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
