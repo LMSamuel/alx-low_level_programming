6d510b85d694f44209b576e10575c3415d467a0b
Sam2968 <masoebesamuel@gmail.com>
2022-10-13 11:21:56 -0700

    4. first puts fn

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/**
+	 * A program that Programming is like building a multilingual puzzle
+	 *
+	 * */
+	puts("Programming is like building a multilingual puzzle");
+
+
+	return (0);
+
+}
