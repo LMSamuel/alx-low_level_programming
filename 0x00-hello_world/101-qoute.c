8fae0bc169d294a3b05a85a1d2efc678679edca4
Sam2968 <masoebesamuel@gmail.com>
2022-10-13 12:54:57 -0700

    write

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point of code and use write fxn
+ * Return: Always 1 if true
+ */
+
+int main(void)
+{
+	int df=1;
+
+	write(df,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
