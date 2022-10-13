8f2427093acd88731bf18b009ad4e5b13035bd25
Sam2968 <masoebesamuel@gmail.com>
2022-10-13 11:36:18 -0700

    4. c-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,11 @@
 int main(void)
 {
 	/**
-	 * A program that Programming is like building a multilingual puzzle
+	 *Description: A program that Programming is like building a multilingual puzzle
 	 *
 	 */
 	puts("\"Programming is like building a multilingual puzzle");
 
 
 	return (0);
-
 }
