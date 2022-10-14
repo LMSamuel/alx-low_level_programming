b976918588adeb615f7cabc0ee9605a80a97d196
Sam2968 <masoebesamuel@gmail.com>
2022-10-14 08:39:22 -0700

    2. convert to lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+/**
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	char result = 'a';
+	
+	while (result <= 'z')
+	{
+		putchar(result);
+		result++;
+	
+	}
+
+	putchar('\n');
+	return (0);
+}
