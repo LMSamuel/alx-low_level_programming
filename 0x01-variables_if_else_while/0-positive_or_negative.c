01bd208a8eaf5478def8867bd38382afc0cd4909
Sam2968 <masoebesamuel@gmail.com>
2022-10-14 08:25:54 -0700

    0.ifelse

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**                                                                                                                                                      
+/**
  * main - prints the last digit of the random
  * number stored in the variable n
  * Return: Always 0 (Succes)
