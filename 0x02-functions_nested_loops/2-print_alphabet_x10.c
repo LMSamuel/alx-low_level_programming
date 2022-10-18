4fdd996797f3fe3db56cb231a2553aef7bb42d70
Sam2968 <masoebesamuel@gmail.com>
2022-10-18 14:14:59 -0700

    print 10times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet- print alpha.
+ * print_alphabet_x10- print alpha.
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
-	int count = 0;
-	
+	int count = 0;	
 	while (count < 10)
 	{
 		char result = 'a';
