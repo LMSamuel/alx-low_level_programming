9622aec8c6caf3ed02c112da5d93bf97e257fba3
Sam2968 <masoebesamuel@gmail.com>
2022-10-19 10:25:14 -0700

    If isalpha print 1 else print 0

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha- print 1 if alpha else print 0.
+ * @c: char to check
+ * Return: Always 1 else 0.
+ */
+int _isalpha(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
