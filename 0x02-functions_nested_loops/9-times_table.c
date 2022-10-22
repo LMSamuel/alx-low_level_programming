c3cbf30c7f5d7e1be63c13159088fc13bd38b982
Sam2968 <masoebesamuel@gmail.com>
2022-10-22 12:28:24 -0700

    9. times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,8 +5,8 @@
  */
 void times_table(void)
 {
-	int i, j ,k;
-	
+	int i, j, k;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
@@ -28,8 +28,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-				
+				_putchar((k % 10) + '0');			
 			}
 		}
 		_putchar('\n');
