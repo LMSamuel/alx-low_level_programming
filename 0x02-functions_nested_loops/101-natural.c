d98ffd29eb5fe682eae7d4025ba0cf24d6da4905
Sam2968 <masoebesamuel@gmail.com>
2022-10-23 01:13:24 -0700

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - prints the n times table, starting with
+ * Return: number of the times table
+ */
+int main(void)
+{
+	unsigned long int sum1, sum2, sum;
+	int i;
+
+	sum1 = 0;
+	sum2 = 0;
+	sum = 0;
+
+	for(int i = 0 ; i < 1024 ; i++)
+	{
+		if((i % 3) == 0)
+		{
+			sum1 =+ i;
+		} else if((i % 5) == 0)
+		{
+			sum2 =+ i;
+		}
+		sum  = sum1 + sum2;
+		printf("%lu\n",sum);
+		return (0);
+
+	}
+}
